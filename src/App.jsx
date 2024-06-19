585f2d3a06aeb3cdc4af59748f64c90045bbf397
chloros <chloros.lin@gmail.com>
2024-06-19 21:52:32 +0800

    completed contact part

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Projects from './components/projects/Projects';
@@ -18,6 +19,7 @@ function App() {
         <Skills />
         <Services />
         <Projects />
+        <Contact />
       </main>
       
     </>
